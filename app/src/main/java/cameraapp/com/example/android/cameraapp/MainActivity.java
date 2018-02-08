d6e89a6ada0e68f12704f97cf6d73d4c42e38b19
Unknown <Zachary.Lynch1337@gmail.com>
2018-02-07 19:40:47 -0800

    Added method to check for cameras

--- app/src/main/java/cameraapp/com/example/android/cameraapp/MainActivity.java
+++ app/src/main/java/cameraapp/com/example/android/cameraapp/MainActivity.java
@@ -1,7 +1,10 @@
 package cameraapp.com.example.android.cameraapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.camera2.*;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        // checks system for camera
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 }
